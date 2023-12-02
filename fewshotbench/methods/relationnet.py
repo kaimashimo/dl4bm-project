762f87743d271509e3a740813a7bd215f628f347
Kai Mashimo <kai.mike.mashimo@gmail.com>
2023-12-02 19:52:01 +0000

    replaced conv relationmodule with linear

--- fewshotbench/methods/relationnet.py
+++ fewshotbench/methods/relationnet.py
@@ -8,47 +8,41 @@ import math
 
 from methods.meta_template import MetaTemplate
 
-
 class RelationNet(MetaTemplate):
     def __init__(self, backbone, n_way, n_support):
         super(RelationNet, self).__init__(backbone, n_way, n_support)
         self.loss_fn = nn.CrossEntropyLoss()
-
-        self.relation_module = RelationModule( self.feat_dim , 8) 
+        self.relation_hidden_size = 128
+        self.relation_module = RelationModule(self.feat_dim * 2, self.relation_hidden_size)
 
     def set_forward(self, x, is_feature=False):
         z_support, z_query = self.parse_feature(x, is_feature)
 
-        z_support = z_support.contiguous()
-        z_support = z_support.view(self.n_way, self.n_support, -1) 
-        z_support = torch.sum(z_support, 1).squeeze(1)
-        
+        z_support = z_support.contiguous().view(self.n_way, self.n_support, -1)
+        z_support = torch.sum(z_support, 1)  # Sum over the support set
+
         z_query = z_query.contiguous().view(self.n_way * self.n_query, -1)
 
-        z_support_ext = z_support.unsqueeze(0).repeat(self.n_query * self.n_way,1,1,1,1)
-        z_query_ext = z_query.unsqueeze(0).repeat(self.n_way,1,1,1,1)
+        z_support_ext = z_support.unsqueeze(0).repeat(self.n_way * self.n_query, 1, 1)
+        z_query_ext = z_query.unsqueeze(1).repeat(1, self.n_way, 1).view(self.n_way * self.n_query, self.n_way, -1)
+        
+        # Dimension of [batch_size, feature_dimension * 2]
+        extend_pairs = torch.cat((z_support_ext, z_query_ext), 2).view(-1, self.feat_dim*2)
 
-        extend_final_feat_dim = self.feat_dim.copy()
-        extend_final_feat_dim[0] *= 2
-
-        extend_pairs = torch.cat((z_support_ext,z_query_ext),2).view(-1, *extend_final_feat_dim)
-        scores =  self.relation_module(extend_pairs ).view(-1,self.n_way)
-
+        scores = self.relation_module(extend_pairs).view(-1, self.n_way)
         return scores
-
     
     def set_forward_loss(self, x):
-        y_query = torch.from_numpy(np.repeat(range( self.n_way ), self.n_query ))
+        y_query = torch.from_numpy(np.repeat(range(self.n_way), self.n_query))
         y_query = Variable(y_query.cuda())
 
         scores = self.set_forward(x)
-
-        return self.loss_fn(scores, y_query )
+        return self.loss_fn(scores, y_query)
 
 
 def weights_init(m):
     classname = m.__class__.__name__
-    if classname.find('Conv') != -1:
+    if classname.find('Conv') != -1 and hasattr(m, 'kernel_size'):
         n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
         m.weight.data.normal_(0, math.sqrt(2. / n))
         if m.bias is not None:
@@ -60,59 +54,30 @@ def weights_init(m):
         n = m.weight.size(1)
         m.weight.data.normal_(0, 0.01)
         m.bias.data = torch.ones(m.bias.data.size())
-
-
-class RelationConvBlock(nn.Module):
-
-    def __init__(self, indim, outdim, padding = 0):
-        super(RelationConvBlock, self).__init__()
-        self.indim  = indim
-        self.outdim = outdim
-        self.C      = nn.Conv2d(indim, outdim, 3, padding = padding )
-        self.BN     = nn.BatchNorm2d(outdim, momentum=1, affine=True)
-        self.relu   = nn.ReLU()
-        self.pool   = nn.MaxPool2d(2)
-
-        self.parametrized_layers = [self.C, self.BN, self.relu, self.pool]
-
-        self.trunk = nn.Sequential(*self.parametrized_layers)
-
-    def forward(self,x):
-        out = self.trunk(x)
-        return out
-    
+    elif classname.find('RelationConvBlock') != -1:
+        conv_layer = m.C
+        n = conv_layer.kernel_size[0] * conv_layer.kernel_size[1] * conv_layer.out_channels
+        conv_layer.weight.data.normal_(0, math.sqrt(2. / n))
+        if conv_layer.bias is not None:
+            conv_layer.bias.data.zero_()
 
 class RelationModule(nn.Module):
 
-    def __init__(self, input_size, hidden_size, padding = 0):
+    def __init__(self, input_size, hidden_size=128):
         super(RelationModule, self).__init__()
         
-        out_layers = input_size[0]* input_size[1] * input_size[2] if type(input_size) == list else input_size
-
-
-        self.conv1 = RelationConvBlock(out_layers, out_layers, padding = padding)
-        self.conv2 = RelationConvBlock(out_layers, out_layers, padding = padding)
-        
-
-        self.fc1 = nn.Linear(out_layers,hidden_size)
-        self.fc2 = nn.Linear(hidden_size,1)
-
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, 1)
         self.relu = nn.ReLU()
+        self.sigmoid = nn.Sigmoid()
 
-        self.layers = self.layers + [self.fc1, self.fc2, self.relu, self.sigmoid]
-        self.layers = nn.Sequential(*self.layers)
-
+        self.layers = nn.Sequential(self.fc1, self.relu, self.fc2, self.sigmoid)
 
         for m in self.modules():
             weights_init(m)
 
     def forward(self, x):
-
-        out = self.conv1(x)
-        out = self.conv2(out)
-        out = out.view(out.size(0),-1)
-        out = self.relu(self.fc1(out))
-        out = self.fc2(out)
+        out = self.layers(x)
         return out
 
 
