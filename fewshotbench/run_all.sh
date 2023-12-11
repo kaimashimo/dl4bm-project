068480fcbbf5990d49f87beec2172cca50782105
Pierre Lardet <lardet.pierre@gmail.com>
2023-12-11 08:11:48 +0000

    run all script

--- fewshotbench/run_all.sh
+++ fewshotbench/run_all.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
 # Define the methods and datasets as arrays
-methods=("protonet" "maml" "matchingnet" "baseline")
+methods=("protonet" "maml" "matchingnet" "baseline" "baseline_pp")
 datasets=("swissprot" "tabula_muris")
-
+n_shots=("1" "5" "10")
+n_ways=("5" "20")
 # Loop through each combination of method and dataset
 for method in "${methods[@]}"; do
     for dataset in "${datasets[@]}"; do
-        echo "Running with method: $method and dataset: $dataset"
-        python run.py exp.name=exp1 method=$method dataset=$dataset
+	for n_shot in "${n_shots[@]}"; do
+	    for n_way in "${n_ways[@]}"; do
+        	echo "Running with method: $method, dataset: $dataset, n_shot: $n_shot, n_way: $n_way"
+        	exp_name=${method}_${dataset}_${n_shot}_${n_way}
+		python run.py exp.name=$exp_name method=$method dataset=$dataset n_shot=$n_shot n_way=$n_way
+	    done
+        done	
     done
 done
