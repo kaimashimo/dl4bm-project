99d6e177e3bdf4caadce85245339e79d6b6dc7ae
Kai Mashimo <kai.mike.mashimo@gmail.com>
2023-12-12 21:33:32 +0000

    preparing for long run

--- fewshotbench/methods/relationnet.py
+++ fewshotbench/methods/relationnet.py
@@ -9,10 +9,10 @@ from methods.meta_template import MetaTemplate
 
 
 class RelationNet(MetaTemplate):
-    def __init__(self, backbone, n_way, n_support):
+    def __init__(self, backbone, n_way, n_support, hidden_size=512, dropout=0.5, n_layers=3):
         super(RelationNet, self).__init__(backbone, n_way, n_support)
         self.loss_fn = nn.CrossEntropyLoss()
-        self.relation_module = RelationModule(input_size = self.feat_dim*2)
+        self.relation_module = RelationModule(input_size = self.feat_dim*2, hidden_size, dropout, n_layers)
 
     def set_forward(self, x, is_feature=False):
         z_support, z_query = self.parse_feature(x, is_feature)
@@ -43,65 +43,27 @@ class RelationNet(MetaTemplate):
 
         return self.loss_fn(scores, y_query )
 
-def weights_init(m):
-    classname = m.__class__.__name__
-    if classname.find('Conv') != -1 and hasattr(m, 'kernel_size'):
-        print('Detected Conv in module name')
-        n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
-        m.weight.data.normal_(0, math.sqrt(2. / n))
-        if m.bias is not None:
-            m.bias.data.zero_()
-    elif classname.find('BatchNorm') != -1:
-        print('Detected BatchNorm in module name')
-        m.weight.data.fill_(1)
-        m.bias.data.zero_()
-    elif classname.find('Linear') != -1:
-        print('Detected Linear in module name')
-        n = m.weight.size(1)
-        m.weight.data.normal_(0, 0.01)
-        m.bias.data.zero_()
-    elif classname.find('RelationConvBlock') != -1:
-        print('Detected RelationConvBlock in module name')
-        conv_layer = m.C
-        n = conv_layer.kernel_size[0] * conv_layer.kernel_size[1] * conv_layer.out_channels
-        conv_layer.weight.data.normal_(0, math.sqrt(2. / n))
-        if conv_layer.bias is not None:
-            conv_layer.bias.data.zero_()
-
 class RelationModule(nn.Module):
-    def __init__(self, input_size, hidden_size=512):
+    def __init__(self, input_size, hidden_size=512, dropout=0.5, n_layers=3, device='cuda'):
         super(RelationModule, self).__init__()
-        self.layer1 = nn.Sequential(nn.Linear(input_size, hidden_size),
-                                    nn.BatchNorm1d(hidden_size),
-                                    nn.ReLU(),
-                                    nn.Dropout(0.40))
-        self.layer2 = nn.Sequential(nn.Linear(hidden_size, hidden_size),
-                                    nn.BatchNorm1d(hidden_size),
-                                    nn.ReLU(),
-                                    nn.Dropout(0.40)) 
-        self.layer3 = nn.Linear(hidden_size, 1)
+        self.device = device
+        self.layers = nn.ModuleList([
+            nn.Sequential(nn.Linear(input_size if i == 0 else hidden_size, hidden_size),
+                          nn.BatchNorm1d(hidden_size),
+                          nn.ReLU(),
+                          nn.Dropout(dropout)) for i in range(n_layers - 1)
+        ])
+        self.layer_final = nn.Linear(hidden_size, 1).to(device)
         self.sigmoid = nn.Sigmoid()
 
+        # Move all layers to the specified device
+        self.layers.to(device)
+
     def forward(self, x):
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
+        x = x.to(self.device)
+        for layer in self.layers:
+            x = layer(x)
+        x = self.layer_final(x)
         x = self.sigmoid(x)
         return x
 
-def cosine_similarity(x, y):
-    # x: N x D
-    # y: M x D
-    n = x.size(0)
-    m = y.size(0)
-    d = x.size(1)
-    assert d == y.size(1)
-
-    # Normalize the rows of x and y
-    x_normalized = F.normalize(x, p=2, dim=1)
-    y_normalized = F.normalize(y, p=2, dim=1)
-
-    # Calculate cosine similarity
-    similarity = torch.mm(x_normalized, y_normalized.transpose(0, 1))
-
-    return -similarity
